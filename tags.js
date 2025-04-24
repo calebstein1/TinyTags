ac813dceaceea75ae9fac5e501911d814063473e
Caleb Stein <caleb@calebstein.net>
2025-04-24 01:19:40 -0700

    enumerate tags

--- tags.js
+++ tags.js
@@ -1,3 +1,19 @@
-function init_tags() {
+const Tags_ElementsList = document.querySelectorAll('[data-tag]');
+let Tags_ActiveTags = 0;
 
+function Tags_InitTags() {
+	const tag_map = {};
+	let i, current = 1;
+
+	for (i = 0; i < Tags_ElementsList.length; i++) {
+		if (!tag_map[Tags_ElementsList[i].dataset.tag]) {
+			tag_map[Tags_ElementsList[i].dataset.tag] = current;
+			Tags_ActiveTags |= current;
+			console.log(`Enumerating tag ${Tags_ElementsList[i].dataset.tag} as ${current}`);
+			current <<= 1;
+		}
+		Tags_ElementsList[i].dataset.tag = tag_map[Tags_ElementsList[i].dataset.tag];
+	}
 }
+
+Tags_InitTags();
